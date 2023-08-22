a37bcec0e92772e5f448a07258049d2c891cedf5
gift kimani <gift.kimani@students.moringaschool.com>
2023-08-22 14:31:32 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq = []
+
+    if length == 0:
+        print([])
+        return 
+    
+    if length >= 1:
+        fib_seq.append(0)
+
+    if length >= 2 :
+        fib_seq.append(1)
+
+    while len(fib_seq) < length :
+        next_fib = fib_seq[-1] + fib_seq[-2] 
+        fib_seq.append(next_fib)
+
+    print(fib_seq)
+
+
+    
